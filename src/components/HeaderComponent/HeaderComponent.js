5ca23f34c126cd25198b03122dcddc017b0d89f9
Sai Darshan <94692595+SaiDarshan2003@users.noreply.github.com>
2023-06-27 11:15:38 +0530

    Delete src directory

--- src/components/HeaderComponent/HeaderComponent.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import './HeaderComponent.css'
-
-const HeaderComponent = () => {
-  return <div className="header">Patient Management App</div>;
-};
-
-export default HeaderComponent;
